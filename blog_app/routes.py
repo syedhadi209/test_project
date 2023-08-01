3b13fc37ca02e9a5e25787e466e8779624918f75
syedhadi209 <hadi.alvi@devsinc.com>
2023-08-01 11:02:14 +0500

    initial commit - hello - bye

--- blog_app/routes.py
+++ blog_app/routes.py
@@ -11,6 +11,11 @@ from cloudinary.utils import cloudinary_url
 
 
 
+@app.before_request
+def before_request():
+    # Initialize the 'g' object with an empty dictionary for each request
+    g.user = {"role":""}
+
 @app.route('/')
 def home():
     return render_template('home.html', title='Home')
